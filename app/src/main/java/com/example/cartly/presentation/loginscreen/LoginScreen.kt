6ba4ee9d3445d022444cc2473d3c2577157cb095
szymonkaczmarek <kaczmarekszymon96@gmail.com>
2026-09-15 21:39:35 +0200

    Implement LoginScreen with UI components and add LoginScreenViewModel

--- app/src/main/java/com/example/cartly/presentation/loginscreen/LoginScreen.kt
+++ app/src/main/java/com/example/cartly/presentation/loginscreen/LoginScreen.kt
@@ -1,36 +1,68 @@
 package com.example.cartly.presentation.loginscreen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.app.ui.theme.AppTheme
+import com.example.cartly.R
 
 @Composable
-fun LoginScreen(){
-  RootView()
-}
-
-@Composable
-private fun RootView(){
+fun LoginScreen() {
     LoginContentScreen()
 }
 
 @Composable
-private fun LoginContentScreen(){
+private fun LoginContentScreen() {
     Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.background),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(
+            modifier = Modifier
+                .size(64.dp)
+                .padding(16.dp)
+                .background(MaterialTheme.colorScheme.background)
+                .clip(MaterialTheme.shapes.extraSmall)
 
-    ){
+        ) {
+            Icon(
+                tint = MaterialTheme.colorScheme.onPrimary,
+                modifier = Modifier
+                    .background(MaterialTheme.colorScheme.primary),
+                painter = painterResource(R.drawable.cartly_icon),
+                contentDescription = "Cartly Icon",
+            )
+        }
+        Text(
+            style = MaterialTheme.typography.headlineMedium,
+            color = MaterialTheme.colorScheme.primary,
+            text = "Carlty"
+        )
 
     }
-    Scaffold(
-        content = TODO()
-    )
 }
 
 
 @Preview
 @Composable
-private fun LoginContentScreenPreview(){
-    LoginContentScreen()
+private fun LoginContentScreenPreview() {
+    AppTheme(
+        content = { LoginContentScreen() }
+    )
 }
 
--- app/src/main/java/com/example/cartly/ui/theme/Theme.kt
+++ app/src/main/java/com/example/cartly/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.app.ui.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
